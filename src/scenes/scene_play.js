52ffbf1caf83aeff51fbb61533d9c1726fcf86d6
Nicolas <nicolas123alejandro@gmail.com>
2021-10-09 23:37:29 -0500

    puzle-show "win" in console

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -14,18 +14,21 @@ class Scene_play extends Phaser.Scene {
 
     constructor() {
         super({ key: "Scene_play" });
+        this.temPosition = {};
     }
 
     init(){
         this.jindu;
         this.sort = randomArr()
-        this.temPosition = {};
+        
         this.pintuGroup
         //Variables del tiempo
         this.time = 50;
         this.timeElapsed = 0;
         this.maxTime = 1;
-        this.tamaSprite = 184;
+        this.tamaSprite = 184/2;
+        this.posX = 560
+        this.posY = 285  
     }
 
     create() {
@@ -35,12 +38,12 @@ class Scene_play extends Phaser.Scene {
         this.game.sound.stopAll();
 
         //this.sound.add('M_puzzle', {loop:true, volume: 0.3}).play();
-        //this.background = this.add.image(center_width, center_height , 'bg-rompecabezas');
+        this.background = this.add.image(center_width, center_height , 'bg-rompecabezas');
 
         //Texto con informacion sobre los dientes
         //this.add.text(center_width-250, center_height-50, 'Un tercio de tus \ndientes está por \ndebajo de la encía.', style);
 
-/*
+
         const buttonMenu = new Button( this, center_width-100, center_height+120, 'button-menu', 'button-menu-hover').setScale(0.6);
         buttonMenu.text.text = 'Regresar';
         this.add.existing(buttonMenu);
@@ -48,10 +51,10 @@ class Scene_play extends Phaser.Scene {
         .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, ()=>{
            this.scene.start('Menu');
         })
-*/
+
         ///=========Puzzle====
 
-        /*
+        
         this.container = this.add.container(530, 145);
         this.jinduBg = this.add.sprite(center_width+150, 220, 'jinduBg').setScale(0.7)
         this.jindu = this.add.sprite(center_width+150, 220, 'jindu').setScale(0.8)
@@ -64,93 +67,91 @@ class Scene_play extends Phaser.Scene {
                 scaleX: { value: 0, duration: this.time * 1000, ease: 'None', repeat: 0 }
             }
         });
-        */
+        
         this.pintuGroup = this.add.group();
         let item;
-        let posX = 540   //560
-        let posY = 150   //285  posiciones para ajustarse al background
+        
         for (var i in this.sort) {
-            item = this.pintuGroup.create(posX +  this.tamaSprite * (Math.floor(i % 3)),   posY + this.tamaSprite * (Math.floor(i / 3)), 'pintu', this.sort[i]);
+            item = this.pintuGroup.create(this.posX +  this.tamaSprite * (Math.floor(i % 3)),   this.posY + this.tamaSprite * (Math.floor(i / 3)), 'pintu', this.sort[i]);
             item.sort = this.sort[i];
             item.nowSort = +i;
-            console.log("principio", item.sort, item.nowSort)
+            //console.log("principio", item.sort, item.nowSort)
             item.setInteractive();
             this.input.setDraggable(item);
             this.input.dragDistanceThreshold = 16;
             this.input.topOnly = false;
 
-            this.input.on('dragstart', function (pointer, gameObject) {
-                gameObject.setDepth(1);
-                gameObject.setTint(0xCCCCCC);
-                this.temPosition.x = gameObject.x;
-                this.temPosition.y = gameObject.y;
-                console.log("partimos de: ", gameObject.x, gameObject.y)
-            }, this);
+            item.on('dragstart', function(pointer, dragX, dragY){
+                this.setDepth(1);
+                this.setTint(0xCCCCCC);
+                
+                this.scene.temPosition.x = this.x;
+                this.scene.temPosition.y = this.y;
+                //console.log("start: ", this.scene.temPosition.x );
+            });
 
-             this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-                 gameObject.x = Phaser.Math.Snap.To(dragX - posX, this.tamaSprite) + posX;
-                 gameObject.y = Phaser.Math.Snap.To(dragY - posY, this.tamaSprite) + posY; 
-             }, this);
+             item.on('drag', function (pointer, dragX, dragY) {
+                 this.x = Phaser.Math.Snap.To(dragX - this.scene.posX, this.scene.tamaSprite) + this.scene.posX;
 
-            this.input.on('dragend', function (pointer, gameObject) {
-                gameObject.setDepth(0);
-                gameObject.clearTint();
-                var temX = (gameObject.x - posX)/(this.tamaSprite);
-                var temY = (gameObject.y - posY)/(this.tamaSprite);
+                 this.y = Phaser.Math.Snap.To(dragY - this.scene.posY, this.scene.tamaSprite) + this.scene.posY; 
+             });
+
+             item.on('dragend', function(pointer, dragX, dragY){
+                this.setDepth(0);
+                this.clearTint();
+                var temX = (this.x - this.scene.posX)/(this.scene.tamaSprite);
+                var temY = (this.y - this.scene.posY)/(this.scene.tamaSprite);
                 if(temX<0 || temX>2 || temY<0 || temY>2){
-                    console.log("Se salio :>")
-                    this.tweens.add({
-                        targets: gameObject,
+                    this.scene.tweens.add({
+                        targets: this,
                         props: {
-                            x: { value: this.temPosition.x, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
-                            y: { value: this.temPosition.y, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
+                            x: { value: this.scene.temPosition.x, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
+                            y: { value: this.scene.temPosition.y, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
                         }
                     });
                     return;
                 }
-                var newSort = (gameObject.x-posX)/this.tamaSprite + (gameObject.y-posY)/this.tamaSprite*3;
-                var ifMoveEnd = true;
-                this.pintuGroup.getChildren().forEach(function(item){
-                    if(item.nowSort === newSort && ifMoveEnd === true){
-                        ifMoveEnd = false;
+
+                var newSort = (this.x- this.scene.posX)/this.scene.tamaSprite + (this.y-this.scene.posY)/this.scene.tamaSprite*3;
+                
+                Phaser.Actions.Call(this.scene.pintuGroup.getChildren(), function(item) {
+                    if(item.nowSort === newSort){
+                        var t = this;
                         item.setDepth(1);
-                        var temtween = this.tweens.add({
+                        this.scene.tweens.add({
                             targets: item,
-                            onComplete: ()=>{
-                                item.nowSort = gameObject.nowSort
-                                gameObject.nowSort = newSort;
-                                ifMoveEnd = true;
-                                console.log("movemos item:", item.sort, item.nowSort)
+                            onComplete: function(){
+                                //console.log(this, this.parent, t.nowSort)
                                 item.setDepth(0);
-                                //this.checkSort();
+                                
+                                item.nowSort = t.nowSort
+                                t.nowSort = newSort;
+                                if(this.parent.scene.checkSort()){
+                                    console.log("GANASTEEE!!!")
+                                }
                             },
                             props: {
-                                x: { value: this.temPosition.x, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
-                                y: { value: this.temPosition.y, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
+                                x: { value: this.scene.temPosition.x, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
+                                y: { value: this.scene.temPosition.y, duration: 300, ease: 'Quartic.Out', repeat: 0 }, 
                             }
-                        });
-                       
-                        
-                        
+                        }, this);
                     }
-                }, this);
 
-                this.checkSort();
-            }, this);
-            
+                  }, this);
+
+             });
         }
 
-        /*
+        
         this.pintuGroup.children.iterate((child) => {
         child.setScale(0.5);
         console.log("Mas peque")
         });
-        */
+        
     }
 
     checkSort(){
         var ifFinash = true;
-        console.log("Revizando ando...")
         this.pintuGroup.getChildren().forEach(function(item) {
             if(item.sort !== item.nowSort){
                 ifFinash = false;
@@ -160,16 +161,15 @@ class Scene_play extends Phaser.Scene {
     }
 
     update(time, delta) {
-        /*
-        let deltaInSecond = delta/1000; // convert it to second
+        let deltaInSecond = delta/1000; 
         this.timeElapsed = this.timeElapsed + deltaInSecond;
-         if(this.timeElapsed >= this.maxTime) // if the time elapsed already more than 1 second, update the energy variable
+         if(this.timeElapsed >= this.maxTime)
         {
             this.time--;
             this.timeText.setText("Tiempo: " + this.time.toString()+ " seg.");
-            this.timeElapsed = 0; // don't forget to reset it to zero again.
-            }
-            */
+            this.timeElapsed = 0; 
+        }
+            
     }
 
 }
